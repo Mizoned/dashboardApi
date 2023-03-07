a39a16dfc3d1d3a2f831c405953c950bc4daff0c
Valery Shcherbinin <fundroin@mail.ru>
2023-03-07 11:58:35 +0500

    В соответствии с измененной модели изменяет userdto

--- dtos/UserDto.js
+++ dtos/UserDto.js
@@ -10,17 +10,41 @@ module.exports = class UserDto {
         dto.email = model.email;
         dto.role = model.role;
         dto.isActivated = model.isActivated;
+        dto.displayName = model.displayName;
+        dto.location = model.location;
+        dto.notifyAboutProductUpdates = model.notifyAboutProductUpdates;
+        dto.notifyAboutMarketNewsletter = model.notifyAboutMarketNewsletter;
+        dto.notifyAboutComments = model.notifyAboutComments;
+        dto.notifyAboutPurchases = model.notifyAboutPurchases;
 
         return dto;
     }
 
     static fromArray(array) {
-        const { id, email, role, isActivated } = array[0];
+        const {
+            id,
+            email,
+            role,
+            isActivated,
+            displayName,
+            location,
+            notifyAboutProductUpdates,
+            notifyAboutMarketNewsletter,
+            notifyAboutComments,
+            notifyAboutPurchases
+        } = array[0];
+
         const dto = new UserDto();
         dto.id = id;
         dto.email = email;
         dto.role = role;
         dto.isActivated = isActivated;
+        dto.displayName = displayName;
+        dto.location = location;
+        dto.notifyAboutProductUpdates = notifyAboutProductUpdates;
+        dto.notifyAboutMarketNewsletter = notifyAboutMarketNewsletter;
+        dto.notifyAboutComments = notifyAboutComments;
+        dto.notifyAboutPurchases = notifyAboutPurchases;
 
         return dto;
     }
