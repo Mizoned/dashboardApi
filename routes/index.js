4f1dcbd920185cd587b3058c6deb91d01262206d
Valery Shcherbinin <fundroin@mail.ru>
2023-06-01 13:48:04 +0500

    Подключает роутер продуктов к общему роутеру

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ const Router = require('express');
 const router = new Router();
 const usersRouter = require('./users/UsersRouter');
 const authRouter = require('./AuthRouter');
+const productsRouter = require('./ProductsRouter');
 
 router.use('/auth', authRouter);
 router.use('/users', usersRouter);
+router.use('/products', productsRouter);
 
 module.exports = router;
