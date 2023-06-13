a374b85af7fc65a2a4a02e14740c6b53b4d9a4d9
Valery Shcherbinin <fundroin@mail.ru>
2023-06-13 21:24:35 +0500

    Подключает маршрутизатор покупок к общему файлу

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,11 @@ const router = new Router();
 const usersRouter = require('./users/UsersRouter');
 const authRouter = require('./AuthRouter');
 const productsRouter = require('./ProductsRouter');
+const purchaseRouter = require('./PurchaseRouter');
 
 router.use('/auth', authRouter);
 router.use('/users', usersRouter);
 router.use('/products', productsRouter);
+router.use('/purchase', purchaseRouter);
 
 module.exports = router;
